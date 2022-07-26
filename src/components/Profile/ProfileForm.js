b427bc6b0597f6d113c19defe282835b896f2cd1
John Doe <sailalith1993@gmail.com>
2022-07-26 15:53:57 +0530

    change to new password

--- src/components/Profile/ProfileForm.js
+++ src/components/Profile/ProfileForm.js
@@ -1,17 +1,70 @@
-import classes from './ProfileForm.module.css';
+import classes from "./ProfileForm.module.css";
+import React, { useContext, useRef, useState } from "react";
+import AuthContext from "../../store/auth-context";
 
 const ProfileForm = () => {
+  const authCtx = useContext(AuthContext);
+  const newPasswordRef = useRef();
+  const [loading, setLoading] = useState(false);
+
+  const newPasswordHandler = (event) => {
+    event.preventDefault();
+    setLoading(true);
+    const enteredNewPassword = newPasswordRef.current.value;
+    let idToken = authCtx.token;
+    let url =
+      "https://identitytoolkit.googleapis.com/v1/accounts:update?key=AIzaSyDovX5U11h_IZTG9O-7IcYVeXe9tpSTIGU";
+
+    fetch(url, {
+      method: "POST",
+      body: JSON.stringify({
+        idToken: idToken,
+        password: enteredNewPassword,
+        returnSecureToken: true,
+      }),
+      headers: { "Content-Type": "application/json" },
+    }).then((res) => {
+      if (res.ok) {
+        return res.json();
+      }
+      // else {
+      //   res.json().then((data) => {
+      //     let errorMessage = "Updating password failed";
+      //     // if (data && data.error && data.error.message) {
+      //     //   errorMessage = data.error.message;
+      //     // }
+      //     console.log(errorMessage);
+      //     alert(errorMessage);
+      //     throw new Error(errorMessage);
+      //   });
+      // }
+    });
+    // .then((data) => {
+    //   console.log(data.idToken);
+    //   authCtx.login(data.idToken);
+    // })
+    // .catch((err) => {
+    //   alert(err.message);
+    // });
+    setLoading(false);
+  };
+
   return (
-    <form className={classes.form}>
+    <form className={classes.form} onSubmit={newPasswordHandler}>
       <div className={classes.control}>
-        <label htmlFor='new-password'>New Password</label>
-        <input type='password' id='new-password' />
+        <label htmlFor="new-password">New Password</label>
+        <input
+          type="password"
+          id="new-password"
+          minLength="5"
+          ref={newPasswordRef}
+        />
       </div>
       <div className={classes.action}>
-        <button>Change Password</button>
+        <button>{loading ? "loading" : "Change Password"}</button>
       </div>
     </form>
   );
-}
+};
 
 export default ProfileForm;
